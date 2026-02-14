a3c20f6ed091043faecdf8b82e2be4b8a839771f
Rizky Ramadhan <jmiryas@gmail.com>
2026-02-14 14:22:51 +0700

    Update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Wajib untuk GitHub Pages
+
+  // Matikan optimasi gambar bawaan Next.js, karena fitur ini butuh server Node.js
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: "/little-universe",
 };
 
 export default nextConfig;
